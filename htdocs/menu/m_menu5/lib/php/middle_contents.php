3002ad282a99006d2bdd0aa1aa019cd4d65e6b4a
hgh04 <gksrjs0425@gmail.com>
2019-11-13 15:57:39 +0900

    마지막 정리

--- htdocs/menu/m_menu5/lib/php/middle_contents.php
+++ htdocs/menu/m_menu5/lib/php/middle_contents.php
@@ -1,13 +1,13 @@
 			<div class='main_screen'>
 				<div class='main_screen_img'>
 					<div class='main_screen_img_text'>
-						메인메뉴3 이미지
+						메인메뉴5 이미지
 						<br>
 						크기 정보 : 250 * 70
 					</div>
 				</div>
 				<div class='main_contents'>
-					메인메뉴3  내용들(디자인 예정)
+					메인메뉴5 내용들(디자인 예정)
 					<br> 
 					크기 정보 : 730 * 500
 				</div>
---
+++ htdocs/menu/m_menu5/lib/php/middle_menu.php
@@ -0,0 +1,7 @@
+		<div class='middle_screen'>
+			<div class='left_menu'>
+				<div class='left_menu_img'>
+					<div class = 'left_menu_img_text'>메인메뉴5 이미지 <br> 크기 정보 : 176 * 71</div>
+				</div>
+			</div>
+			
