1d879582e22ad88d55ea97c3fda0e8f99375a039
hgh04 <gksrjs0425@gmail.com>
2019-11-14 18:27:29 +0900

    html 정리 추가

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,11 +1,17 @@
-<?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
-?>
-Something is wrong with the XAMPP installation :-(
+<html>
+	<title></title>
+	<head>
+	
+	</head>
+	
+	<body>
+		<img src='test.png' style='width:50px; height:200px;'>
+		
+		<a href="http://www.naver.com" target='test_target'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp네이버</a>
+		<a target='test_target'>네이버</a>
+		
+		<div onclick="location.href='http://www.naver.com'">&nbsp예시입니다.</div>
+
+	</body>
+
+</html>
