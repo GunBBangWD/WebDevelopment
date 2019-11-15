cb7530f1694b65805bd315ecb7792e7c4b3a4477
hgh04 <gksrjs0425@gmail.com>
2019-11-15 13:23:46 +0900

    웹관련 내용 정리중 (html 70% 정도 정리 완료)

--- htdocs/index.php
+++ htdocs/index.php
@@ -2,15 +2,24 @@
 	<title></title>
 	<head>
 	
+	<meta name="subject" content="홈짱닷컴 메타태그(Meta tag) 강좌">
+	<meta name="title" content="메타 태그(Meta tag)">
+	<meta name="author" content="homejjang">
+	<meta name="keywords" content="meta태그, HTML, 홈페이지제작">
+	
 	</head>
 	
 	<body>
-		<img src='test.png' style='width:50px; height:200px;'>
+		<table>
+			<tr>
+				<td>1*1 테이블</td>
+				<td>1*2 테이블</td>
+			</tr>
+			<tr>
+				<td>2*1 테이블</td>
+			</tr>
+		</table>
 		
-		<a href="http://www.naver.com" target='test_target'>&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp네이버</a>
-		<a target='test_target'>네이버</a>
-		
-		<div onclick="location.href='http://www.naver.com'">&nbsp예시입니다.</div>
 
 	</body>
 
