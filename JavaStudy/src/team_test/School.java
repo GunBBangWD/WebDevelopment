ef2cc5be0ec0c893494c20bcc75fe48aa2805277
DESKTOP-FK89NF0\hgh04 <gbwebdeveloper0425@gmail.com>
2023-04-28 21:06:43 +0900

    웹개발 관련 학습 초급단계 마무리

--- JavaStudy/src/team_test/School.java
+++ JavaStudy/src/team_test/School.java
@@ -5,29 +5,21 @@ import java.util.ArrayList;
 public class School {
     ArrayList<Student> students = new ArrayList<Student>();
     ArrayList<Classroom> classrooms = new ArrayList<Classroom>();
-
-
-
-
     public void addStudent(Student student) {
         students.add(student);
     }
     public void addClassroom(Classroom classroom) {
         classrooms.add(classroom);
     }
-
     public void printAll(){
         ArrayList<Student> printList = new ArrayList<>();
         printList.addAll(students);
         for (Classroom c : classrooms){
             printList.addAll(c.students);
         }
-
         for (Student c : students){
             System.out.println(c);
         }
-
-
     }
 
 }
